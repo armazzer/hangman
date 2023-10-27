322778c5c10eafd150b70697188557a8fe061119
armazzer <alice.mazzer.88@gmail.com>
2023-10-27 18:31:09 +0100

    Updates milestone_2.py. Code refactored into two functions: generate_word and request_letter.

--- milestone_2.py
+++ milestone_2.py
@@ -1,10 +1,18 @@
+
 import random
-word_list = ["strawberry", "rasberry", "mango", "peach", "cherry"]
-print(word_list)
-word = random.choice(word_list)
-print(word)
-guess = input("Please enter a letter of your choice ")
-if len(guess) == 1 and guess.isalpha() == True:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input")
+
+def generate_word():
+    word_list = ["strawberry", "rasberry", "mango", "peach", "cherry"]
+    print(word_list)
+    word = random.choice(word_list)
+    print(word)
+
+def request_letter():
+    player_guess = input("Please enter a letter of your choice ")
+    if len(player_guess) == 1 and player_guess.isalpha() == True:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input")
+
+generate_word()
+request_letter()
